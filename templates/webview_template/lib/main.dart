546cd5a1e8a0d47f44c9c5f074b05ed91bde49d8
zenoss2311 <zenoss.2311@gmail.com>
2026-06-30 23:27:24 +0700

    🔧 Setup: templates/webview_template/lib/main.dart

---
+++ templates/webview_template/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewPage(url: "__WEB_URL__"),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  final String url;
+  const WebViewPage({super.key, required this.url});
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: WebViewWidget(controller: controller)),
+    );
+  }
+}
